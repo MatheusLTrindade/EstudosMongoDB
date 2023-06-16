67bbf9452bd2756ccca0a3cb9a5756b67132dc8a
Matheus Lopes Trindade <matheus.lopes.trindade@hotmail.com>
2023-06-16 06:55:38 -0300

    add configuração  para conexão ao MongoDB

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list')
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch((err) => console.log(err))
